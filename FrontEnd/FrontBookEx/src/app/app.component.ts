a145b6e7a964c76a5f554e2a9f0bfa2e8500c9d3
annavispo <annavispo.dev@gmail.com>
2025-04-20 12:16:24 +0200

    prima bozza navbar

--- FrontEnd/FrontBookEx/src/app/app.component.ts
+++ FrontEnd/FrontBookEx/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SignUpComponent],
+  imports: [SignUpComponent, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'BookExchange';
+  constructor(public router: Router) {}
+
+  get mostraNavbar(): boolean {
+    // const path = this.router.url;
+    // return !(path === '/login' || path === '/sign-up');
+    return true;
+  }
 }
